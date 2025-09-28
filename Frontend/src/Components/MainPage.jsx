f46b4aba2163cf23372ae91e3cf8ba13c543f254
Shreshth <shreshthkanchan@gmail.com>
2025-09-29 00:17:55 +0530

    ----

---
+++ Frontend/src/Components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../../public/css/output.css";
+
+const MainPage = () =>{
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default MainPage;
