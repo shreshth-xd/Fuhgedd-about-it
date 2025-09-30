315278d1d19ab5bdf7aec1887858a387b62d258f
Shreshth <shreshthkanchan777@gmail.com>
2025-09-30 18:26:42 +0530

    Main page

--- Frontend/src/Components/MainPage.jsx
+++ Frontend/src/Components/MainPage.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import {useState, useRef, useCallback, useMemo} from "react";
 import "../../../public/css/output.css";
 
 const MainPage = () =>{
     return(
         <div>
-            
+            <h1>This is the main page</h1>
         </div>
     );
 }
