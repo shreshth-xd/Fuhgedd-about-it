5756c079513325ebce5fec69c3de84350a315c29
Shreshth <shreshthkanchan777@gmail.com>
2025-10-01 11:46:41 +0530

    Added some validation on each key

--- Models/User.mjs
+++ Models/User.mjs
@@ -2,15 +2,20 @@ import mongoose, { Schema } from "mongoose";
 
 const userSchema = new mongoose.Schema({
     username: {
-        type: String
+        type: String,
+        required: true,
+        unique: true
     },
     email:{
-        type: String
+        type: String,
+        required: true,
+        unique: true
     },
     password:{
-        type: String
+        type: String,
+        required: true,
+        minLength:8
     }
-
 })
 
 export const User = mongoose.model("user", userSchema);
