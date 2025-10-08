bdcd19887d178d9a2907f3afcdd7ff441a72407d
Shreshth <shreshthkanchan777@gmail.com>
2025-10-09 00:01:56 +0530

    Route to be served for /user requests

--- Routers/UsersRouter.js
+++ Routers/UsersRouter.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const signUp = require("../Controllers/UserControllers")
-
 const app = express();
 const router = express.Router();
 
 
+router.post("/", signUp)
 
 module.exports = router;
