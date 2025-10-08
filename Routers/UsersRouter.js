04861386e9a627e33e0ccdda3464e4d43b66f6fc
Shreshth <shreshthkanchan777@gmail.com>
2025-10-08 23:15:01 +0530

    Route to serve user related requests

---
+++ Routers/UsersRouter.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const signUp = require("../Controllers/UserControllers")
+
+const app = express();
+const router = express.Router();
+
+
+
+module.exports = router;
