bfdf7fee1ade32565f016939070e504599780594
Shreshth <shreshthkanchan777@gmail.com>
2025-10-15 00:55:01 +0530

    Main App interface

---
+++ Frontend/src/Pages/MainPage.jsx
@@ -0,0 +1,16 @@
+import {useState, useRef, useCallback, useMemo} from "react";
+import "../../../public/css/output.css";
+
+const MainPage = () =>{
+    return(
+        <div>
+            <nav className="w-10/12 p-2.5 ">
+                <div className="primary-buttons">
+
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default MainPage;
