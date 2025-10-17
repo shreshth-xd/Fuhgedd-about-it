bacced6e48830d4040e1c6c755c46d428abddb28
Shreshth <shreshthkanchan777@gmail.com>
2025-10-17 21:12:45 +0530

    Authentication middleware to gatekeep the protected routes

---
+++ Middlewares/Authentication.js
@@ -0,0 +1,16 @@
+const {setUser, getUser} = require("../Services/Auth")
+
+async function RestrictToLoggedInUsersOnly(req, res, next){
+    const userSessionId = req.cookies.uid;
+    if(!userSessionId) res.redirect("http://localhost:5173/sign-in")
+    
+    const user = getUser(userSessionId);
+    if(!user){
+        return res.redirect("http://localhost:5173/sign-in")
+    }
+
+    req.user = user;
+    next();    
+}
+
+module.exports = {RestrictToLoggedInUsersOnly}
