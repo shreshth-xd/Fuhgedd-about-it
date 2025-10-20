157a6791965ce608ba45ca10bb4c4f8f75241951
Shreshth <shreshthkanchan777@gmail.com>
2025-10-20 11:59:28 +0530

    Debugged to retrieve clearer payloads

--- Services/JWTAuth.js
+++ Services/JWTAuth.js
@@ -1,14 +1,19 @@
 const jwt = require("jsonwebtoken")
 const signature = "universalSignature123$#"
 
-async function setUser(user){
-    return jwt.sign(user, signature)
+function setUser(payload){
+    return jwt.sign({
+        id: payload._id,
+        username: payload.username
+    }, signature, {
+        expiresIn: "15d"
+    })
 }
 
-async function getUser(token){
+function getUser(token){
     return jwt.verify(token, signature)
 }
 
 module.exports = {
-    setUser
+    setUser, getUser
 }
