0fc3498a38d4caf662bb8bf38097d76b648e65f2
Shreshth <shreshthkanchan@gmail.com>
2025-10-20 11:57:43 +0530

    Sign in and Sign up controllers

--- Controllers/UserControllers.js
+++ Controllers/UserControllers.js
@@ -1,11 +1,14 @@
 const path = require("node:path")
 const {User} = require("../Models/User.mjs")
-const publicDir = path.join(__dirname, "Frontend/dist");
+const publicDir = path.join("../", "Frontend/dist");
+const {setUser, getUser} = require("../Services/JWTAuth")
+const {bcrypt} = require("bcrypt")
 
 async function signUp(req, res){
     try{
         const {username, email, password} = req.body;
-        let newUser = new User({username, email, password})
+        const hashedPassword = await bcrypt.hash(password, 10)
+        let newUser = new User({username, email, password: hashedPassword})
         await newUser.save()
         return res.json({"Status": "User created successfully"})
     }catch(error){
@@ -16,11 +19,17 @@ async function signUp(req, res){
 async function signIn(req, res){
     try{
         const {username, password} = req.body;
-        let isUser = await User.findOne({username, password})
-        if(!isUser){
-            res.render("Onboarding", {title: "Login", showSignIn: true, showSignUp: false})
+        let user = await User.findOne({username, password})
+        if(!user || !(await bcrypt.compare(password, user.password))){
+            res.status(404).json({"Status":"User not found"})
         }else{
-            res.sendFile("index.html", {root: publicDir})
+            const token = setUser({id: user._id, username: user.username})
+            res.cookie("JWT_token", token, {
+                maxAge: 15 * 24 * 60 * 60 * 1000,
+                httpOnly: false,
+                sameSite: lax
+            })
+            res.status(200).json({"Status":"Signed in successfully"})
         }
     }catch(error){
         console.log(error)
