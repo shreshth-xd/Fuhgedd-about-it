0b0938d5e20608003b241b353068255485499ea1
Shreshth <shreshthkanchan@gmail.com>
2025-10-22 01:14:33 +0530

    Controller for vaults

---
+++ Controllers/VaultController.js
@@ -0,0 +1,8 @@
+const path = require("node:path");
+const {Vault} = require("../Models/Vault.mjs")
+const {User} = require("../Models/User.mjs")
+
+// Write a controller to see if user has even created any vault by so far or not
+async function GetVaults(req, res){
+    
+}
