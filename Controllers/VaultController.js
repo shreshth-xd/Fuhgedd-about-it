70c162611bcb99aa937f22e0b6a23807c92fcb00
Shreshth <shreshthkanchan@gmail.com>
2025-10-22 20:27:37 +0530

    Wrapped up the controller logic in a try-catch statement

--- Controllers/VaultController.js
+++ Controllers/VaultController.js
@@ -6,13 +6,22 @@ const jwt = require("jsonwebtoken")
 // Write a controller to see if user has even created any vault by so far or not
 async function GetVaults(req, res){
     const token = req.cookies?.JWT_token;
-    console.log(token)
-    const decoded = jwt.verify(token, process.env.JWT_SIG)
-    let vaults = Vault.find({_id: decoded.id, user: decoded.username})
-    if(vaults.length==0){
-        res.status(404).json({"Status":"No vaults found here"})
-    }else{
-        res.status(200).json({"Status":"Successfully fetched the vaults"})
+    if(!token){
+        return res.status(404).json({Status: "JWT token not found"})
+    }
+
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SIG)
+        console.log(decoded)
+        let vaults = await Vault.find({_id: decoded.id, user: decoded.username})
+        if(vaults.length==0){
+            return res.status(404).json({"Status":"No vaults found here"})
+        }else{
+            return res.status(200).json({"Status":"Successfully fetched the vaults"})
+        }
+    }catch(error){
+        return res.status(200).json({Status:"The JWT token is either invalid or expired"})
     }
 }
 
