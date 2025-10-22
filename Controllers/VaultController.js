12bbf43dc3710aa61466cdc7be06f5fdaff7a8f3
Shreshth <shreshthkanchan@gmail.com>
2025-10-23 00:49:32 +0530

    Testing and cross checking the JWT's payload

--- Controllers/VaultController.js
+++ Controllers/VaultController.js
@@ -6,22 +6,25 @@ const jwt = require("jsonwebtoken")
 // Write a controller to see if user has even created any vault by so far or not
 async function GetVaults(req, res){
     const token = req.cookies?.JWT_token;
+    console.log(token)
     if(!token){
         return res.status(404).json({Status: "JWT token not found"})
     }
 
 
     try{
-        const decoded = jwt.verify(token, process.env.JWT_SIG)
+        const decoded = jwt.decode(token, {complete: true})
         console.log(decoded)
-        let vaults = await Vault.find({_id: decoded.id, user: decoded.username})
+        console.log(token==req.cookies?.JWT_token)
+        
+        let vaults = await Vault.find({user: decoded.username})
         if(vaults.length==0){
             return res.status(404).json({"Status":"No vaults found here"})
         }else{
             return res.status(200).json({"Status":"Successfully fetched the vaults"})
         }
     }catch(error){
-        return res.status(200).json({Status:"The JWT token is either invalid or expired"})
+        return res.status(200).json({Status:error})
     }
 }
 
