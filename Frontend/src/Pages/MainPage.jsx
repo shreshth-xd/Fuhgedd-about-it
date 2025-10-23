f7a2f7c6596fd5bdb017a5fa06ff3cb099552e95
Shreshth <shreshthkanchan777@gmail.com>
2025-10-24 00:00:12 +0530

    Just a little UI tweak for the dialog box

--- Frontend/src/Pages/MainPage.jsx
+++ Frontend/src/Pages/MainPage.jsx
@@ -33,59 +33,83 @@ const MainPage = () => {
 
     return (
         <>
-        <div className="ParentFlexBox bg-linear-258 from-[#FF8400] to-[#FFF8AA] w-screen h-screen flex flex-col">
-        <AppNavBar/>
+        <div className="ParentFlexBox bg-linear-258 from-[#FF8400] to-[#FFF8AA] w-full h-screen flex flex-col">
+            <AppNavBar/>
 
-        <div className="main-page">
-            {status === "success" && (
-                <div className="vaults">
-                    <h1>Vaults will display here</h1>
-                    {/* Render vaults dynamically */}
-                    {vaultBoxes.map((vault, index) => (
-                        <div key={index} className="vault-box">
-                            <p>{vault.name}</p>
-                        </div>
-                    ))}
-                </div>
-            )}
+            <div className="main-page">
+                {status === "success" && (
+                    <div className="vaults">
+                        <h1>Vaults will display here</h1>
+                        {/* Render vaults dynamically */}
+                        {vaultBoxes.map((vault, index) => (
+                            <div key={index} className="vault-box">
+                                <p>{vault.name}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
 
-            {status === "empty" && (
-                <div className="NoVaultParent w-full mx-auto h-72">
-                    <div className="NoVaultBox h-full w-96">
-                        <div className="UpperBar pt-2 px-2 w-full h-14 flex items-center justify-end">
-                            <svg
-                                width="35"
-                                height="43"
-                                viewBox="0 0 35 43"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    d="M17.5 0.5C26.8757 0.5 34.5 8.32165 34.5 18C34.5 27.6784 26.8757 35.5 17.5 35.5C8.12434 35.5 0.5 27.6784 0.5 18C0.5 8.32165 8.12434 0.5 17.5 0.5Z"
-                                    fill="#F4FFAA"
-                                    stroke="#F4FFAA"
-                                />
-                                <path
-                                    d="M14.8746 27H10.3919L14.8609 19.046L10.5969 10.9827H15.9816L17.1843 14.003L17.7856 15.7387L18.4416 14.0303L19.7399 10.9827H24.2089L19.9586 18.2123L24.4003 27H19.0976L17.6216 23.31L17.1023 21.6427L16.4326 23.31L14.8746 27Z"
-                                    fill="#0B0C00"
-                                />
-                            </svg>
-                        </div>
-                        <div className="dialog h-52 w-full text-center">
-                            <h2 className="jersey-25">No vaults to be found here</h2>
-                            <p>Click to get started</p>
+                {status === "empty" && (
+                    <div className="NoVaultParent w-full mx-auto h-72">
+                        <div className="NoVaultBox h-full w-96">
+                            <div className="UpperBar pt-2 px-2 w-full h-14 flex items-center justify-end">
+                                <svg
+                                    width="35"
+                                    height="43"
+                                    viewBox="0 0 35 43"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M17.5 0.5C26.8757 0.5 34.5 8.32165 34.5 18C34.5 27.6784 26.8757 35.5 17.5 35.5C8.12434 35.5 0.5 27.6784 0.5 18C0.5 8.32165 8.12434 0.5 17.5 0.5Z"
+                                        fill="#F4FFAA"
+                                        stroke="#F4FFAA"
+                                    />
+                                    <path
+                                        d="M14.8746 27H10.3919L14.8609 19.046L10.5969 10.9827H15.9816L17.1843 14.003L17.7856 15.7387L18.4416 14.0303L19.7399 10.9827H24.2089L19.9586 18.2123L24.4003 27H19.0976L17.6216 23.31L17.1023 21.6427L16.4326 23.31L14.8746 27Z"
+                                        fill="#0B0C00"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="dialog h-52 w-full text-center">
+                                <h2 className="jersey-25">No vaults to be found here</h2>
+                                <p>Click to get started</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            )}
+                )}
 
-            {status === "error" && (
-                <div className="error-message">
-                    <p>There was an error fetching the vaults. Please try again later.</p>
-                </div>
-            )}
+                {status === "error" && (
+                                <div className="NoVaultParent w-full h-auto mt-6">
+                                    <div className="NoVaultBox h-72 w-96 mx-auto">
+                                        <div className="UpperBar pt-2 px-2 w-full h-14 flex items-center justify-end border-2 border-black">
+                                            <svg
+                                                width="35"
+                                                height="43"
+                                                viewBox="0 0 35 43"
+                                                fill="none"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path
+                                                    d="M17.5 0.5C26.8757 0.5 34.5 8.32165 34.5 18C34.5 27.6784 26.8757 35.5 17.5 35.5C8.12434 35.5 0.5 27.6784 0.5 18C0.5 8.32165 8.12434 0.5 17.5 0.5Z"
+                                                    fill="#F4FFAA"
+                                                    stroke="#F4FFAA"
+                                                />
+                                                <path
+                                                    d="M14.8746 27H10.3919L14.8609 19.046L10.5969 10.9827H15.9816L17.1843 14.003L17.7856 15.7387L18.4416 14.0303L19.7399 10.9827H24.2089L19.9586 18.2123L24.4003 27H19.0976L17.6216 23.31L17.1023 21.6427L16.4326 23.31L14.8746 27Z"
+                                                    fill="#0B0C00"
+                                                />
+                                            </svg>
+                                        </div>
+                                        <div className="dialog h-32 w-full text-center border-2 border-black rounded-b-lg pt-8">
+                                            <h2 className="jersey-25">There was an error in retrieving the vaults</h2>
+                                            <p>Please try again later</p>
+                                        </div>
+                                    </div>
+                                </div>
+                )}
+            </div>
         </div>
-    </div>
 
         
         </>
