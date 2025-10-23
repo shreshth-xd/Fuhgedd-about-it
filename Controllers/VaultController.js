7cffdad69c214fef4bb4173240b4e3f96f0e4f8b
Shreshth <shreshthkanchan777@gmail.com>
2025-10-23 23:59:10 +0530

    Removed recurring logs

--- Controllers/VaultController.js
+++ Controllers/VaultController.js
@@ -2,17 +2,17 @@ const path = require("node:path");
 const {Vault} = require("../Models/Vault.mjs")
 const {User} = require("../Models/User.mjs")
 const jwt = require("jsonwebtoken")
+const {getUser} = require("../Services/JWTAuth")
 
 // Write a controller to see if user has even created any vault by so far or not
 async function GetVaults(req, res){
     const token = req.cookies?.JWT_token;
-    console.log(token)
     if(!token){
         return res.status(404).json({Status: "JWT token not found"})
     }
 
     try{
-        const decoded = jwt.verify(token, process.env.JWT_SIG)
+        const decoded = getUser(token) 
         let vaults = await Vault.find({user: decoded.username})
         if(vaults.length==0){
             return res.status(404).json({"Status":"No vaults found here"})
