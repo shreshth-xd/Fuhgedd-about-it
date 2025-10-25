f58de7649b3eb60a1a3ed157932f753a6d44d137
Shreshth <shreshthkanchan777@gmail.com>
2025-10-26 01:17:06 +0530

    Creating a dialog box to let the user create their vault

--- Frontend/src/Components/DialogBox.jsx
+++ Frontend/src/Components/DialogBox.jsx
@@ -9,8 +9,8 @@ const DialogBox = ({ isOpen, onClose, children }) => {
         <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center z-50">
                 {/* Dialog box */}
             <div className="bg-[#100f0f] text-white p-5 rounded-xl shadow-2xl w-2/4 relative">
-                <div className="DialogHeader w-full flex items-center justify-between mb-2.5">
-                    <h1>Vault details</h1>
+                <div className="DialogHeader w-full flex items-center justify-between mb-5">
+                    <h1 className="text-2xl">Vault details</h1>
                     <button onClick={onClose} className="hover:font-semibold px-2.5 py-1 rounded-2xl bg-white text-black lg:text-white lg:bg-black/50 hover:bg-white hover:text-black">
                         ✕
                     </button>
