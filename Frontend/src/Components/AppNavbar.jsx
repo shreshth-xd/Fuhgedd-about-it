3a5144a3f9d2c41ab17eee7b6932b3177fd1d918
Shreshth <shreshthkanchan777@gmail.com>
2025-10-25 17:45:19 +0530

    Changed the color scheme

--- Frontend/src/Components/AppNavbar.jsx
+++ Frontend/src/Components/AppNavbar.jsx
@@ -2,19 +2,19 @@ import { useState } from "react";
 
 const AppNavBar = () =>{
     return(
-        <div className="navBar jersey-25 bg-[#FFF8AA] mt-2 mx-auto w-11/12 px-12 py-3 flex items-center justify-between border-[3px] border-black rounded-3xl">
+        <div className="navBar jersey-25 bg-[#fbffaa] mt-2 mx-auto w-11/12 px-12 py-3 flex items-center justify-between border-[3px] border-black rounded-3xl">
             <div className="VaultsAndSettings h-full flex items-center gap-x-2">
                 <button className="px-7 py-3.5 rounded-2xl navbar-vault-btn border-2 border-black 
                 {if(window.location.href='/app'){
-                    bg-[#ff8400]
+                    bg-[#e2ea14]
                 }}
                 ">Vaults</button>
                 <button className="px-7 py-3.5 rounded-2xl navbar-settings-btn border-2 border-black 
-                bg-linear-90 from-[#FFF8AA_30%] to-[#FF8400]">Settings</button>
+                bg-linear-90 from-[#fff9aa_30%] to-[#fff700]">Settings</button>
             </div>
             <div className="Logout-btn h-full">
                 <button className="px-7 py-3.5 rounded-2xl border-2 border-black 
-                bg-linear-90 from-[#FFF8AA_30%] to-[#FF8400]
+                bg-linear-90 from-[#fff9aa_30%] to-[#fff700]
                 ">Logout</button>
             </div>
         </div>
