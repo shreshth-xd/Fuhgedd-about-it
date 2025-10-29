93bc49e4359fc9b191a815ce594aa0d3890fbb38
Shreshth <shreshthkanchan777@gmail.com>
2025-10-29 23:05:57 +0530

    Disabled the strict mode for a while

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,7 +5,7 @@ import "../../public/css/output.css"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <App />
-  </StrictMode>,
+  // </StrictMode>
 )
