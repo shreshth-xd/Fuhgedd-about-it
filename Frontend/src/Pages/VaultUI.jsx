c46305e0d1337103988097e89ef5786c15c8256c
Shreshth <shreshthkanchan777@gmail.com>
2025-11-06 00:13:31 +0530

    Added bg color

--- Frontend/src/Pages/VaultUI.jsx
+++ Frontend/src/Pages/VaultUI.jsx
@@ -2,7 +2,12 @@ import { useState } from "react";
 
 const Vault = () =>{
     return(
-        <></>
+        <>
+            <div className="ParentContainer flex justify-between items-center gap-x-3 h-[100vh] w-[100vw] m-0 p-8 bg-[linear-gradient(257.89deg,rgba(0,0,0,0.83)_25.09%,rgba(49,49,49,0.83)_85.57%)]">
+                <div className="SideBar"></div>
+                <div className="FrontElements"></div>
+            </div>
+        </>
     )
 }
 
