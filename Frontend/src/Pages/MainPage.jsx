9b3a08be3d9d32ed8d33d91834dcd20c6ccddf81
Shreshth <shreshthkanchan777@gmail.com>
2025-11-05 20:43:39 +0530

    Refactored the way in which the vaults are being displayed

--- Frontend/src/Pages/MainPage.jsx
+++ Frontend/src/Pages/MainPage.jsx
@@ -4,12 +4,12 @@ import "../Components/Home.css";
 import AppNavBar from "../Components/AppNavbar";
 import DialogBox from "../Components/DialogBox";
 import { v4 as uuidv4 } from "uuid";
-// import {Key} from "lucide-react"
 import ErrorBox from "../Components/PopUp";
 
-// Delete icon
+// React icons
 import { MdDelete } from "react-icons/md";
-
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdDeleteSweep } from "react-icons/md";
 
 const MainPage = () => {
     const [vaultBoxes, setVaultBoxes] = useState([]);
@@ -97,7 +97,7 @@ const MainPage = () => {
 
             if(res.ok){
                 // Gonna update the vaultBoxes state as soon as we receive the vaults on the assurance of our request:
-
+                setStatus("")
                 setVaultBoxes(currVaultsOnPage => {
                 
                     // The data has to be in such a way that I can use it to make the vault component
@@ -118,6 +118,8 @@ const MainPage = () => {
         }
         
     }
+
+
 
     useEffect(() => {
         const fetchVaults = async () => {
@@ -144,56 +146,66 @@ const MainPage = () => {
 
     return (
         <>
-        <div className="ParentFlexBox bg-[#232323] w-full h-screen flex flex-col gap-y-5 relative">
+        <div className="ParentFlexBox bg-[linear-gradient(135deg,#1f1f1f,#2c2c2c)] w-full h-screen flex flex-col gap-y-5 relative">
             <AppNavBar/>
 
-            <div className="main-page max-w-full h-full ">
-                {status === "success" && (
-                    <div className="vaults">
-                        <h1>Vaults will display here</h1>
-                        {/* Render vaults dynamically */}
-                        {vaultBoxes.map((vault, index) => (
-                            <div key={index} className="vault-box">
-                                <p>{vault.name}</p>
-                            </div>
-                        ))}
-                    </div>
-                )}
-
-                {status === "empty" && (
-                    <div className="NoVaultParent w-full h-auto mt-34">
-                        <div className="NoVaultBox h-72 w-96 mx-auto">
-                            <div className="UpperBar pt-2 px-2 w-full h-14 flex items-center justify-end border-2 border-black bg-linear-90 from-[#1a4bbf] to-[#020887]">
-                                <svg width="35" height="43" viewBox="0 0 35 43" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M17.5 0.5C26.8757 0.5 34.5 8.32165 34.5 18C34.5 27.6784 26.8757 35.5 17.5 35.5C8.12434 35.5 0.5 27.6784 0.5 18C0.5 8.32165 8.12434 0.5 17.5 0.5Z" fill="#F4FFAA" stroke="#F4FFAA"/>
-                                    <path d="M15.4229 27V18.1577L14.6029 17.652V16.1897L17.5413 15.3833H19.8236V27H15.4229ZM17.4046 14.126L15.1633 11.871V11.3927L17.4046 9.15133H17.9239L20.2609 11.3927V11.871L17.9239 14.126H17.4046Z" fill="#0B0C00"/>
-                                </svg>
-                            </div>
-                            <div className="dialog h-32 w-full text-center border-2 border-black rounded-b-lg pt-8 bg-linear-90 from-[#1a4bbf] to-[#020887_97.12%]">
-                                <h2 className="jersey-25 text-gray-950">No vaults to be found here</h2>
-                                <p className="hover:text-white text-white md:text-black hover:font-medium inline" onClick={()=>{return setIsVaultBoxOpen(true)}}>Click to get started</p>
-                            </div>
-                        </div>
-                    </div>
-                )}
-
-                {status === "error" && (
-                                <div className="NoVaultParent w-full h-auto mt-34 text-white">
-                                    <div className="NoVaultBox h-72 w-96 mx-auto">
-                                        <div className="UpperBar pt-2 px-2 w-full h-14 flex items-center justify-end border-2 border-black bg-linear-90 from-[#1a4bbf] to-[#020887]">
-                                            <svg width="35" height="43" viewBox="0 0 35 43" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path d="M17.5 0.5C26.8757 0.5 34.5 8.32165 34.5 18C34.5 27.6784 26.8757 35.5 17.5 35.5C8.12434 35.5 0.5 27.6784 0.5 18C0.5 8.32165 8.12434 0.5 17.5 0.5Z" fill="#F4FFAA" stroke="#F4FFAA"/>
-                                                <path d="M15.4229 27V18.1577L14.6029 17.652V16.1897L17.5413 15.3833H19.8236V27H15.4229ZM17.4046 14.126L15.1633 11.871V11.3927L17.4046 9.15133H17.9239L20.2609 11.3927V11.871L17.9239 14.126H17.4046Z" fill="#0B0C00"/>
-                                            </svg>
-                                        </div>
-                                        <div className="dialog h-32 w-full text-center border-2 border-black rounded-b-lg pt-8">
-                                            <h2 className="jersey-25">There was an error in retrieving the vaults</h2>
-                                            <p>Please try again later</p>
+            <div className="main-page w-full h-full">
+                <div className="vaults border-2 border-white lg:w-6xl xl:w-7xl mx-auto p-6 h-full overflow-hidden rounded-4xl bg-linear-180 from-[#161616] to-[#313131]">
+                    {status === "success" && (
+                        <div className="flex flex-col gap-y-2.5">
+                            {vaultBoxes.map((vault) => (    
+                                    <div key={vault._id} className="vault-box h-auto w-full bg-[#2c2c2c] px-3 py-2 rounded-[6px]">
+                                        <div className="dialog w-full flex justify-between items-center text-gray-200">
+                                            <h2 className="" onClick={()=>{window.location.href = "/vault"}}>{vault.name}</h2>
+                                            <div className="flex gap-x-1.5">
+                                                <button onClick={()=>removeCred(cred.id)} className="DeleteBtn p-2 flex items-center justify-center form-btn xl:h-[35px] xl:w-[90px] md:text-white bg-red-600 text-white hover:font-semibold rounded-[4px] mb-1"><MdDelete/></button>
+                                                <button className="CredSettingsBtn p-2 flex items-center justify-center form-btn xl:h-[35px] xl:w-[90px] md:text-black bg-white text-black hover:font-semibold rounded-[4px] mb-1"><IoSettingsOutline/></button>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                )}
+                            ))}
+                            <div className="AddIcon h-auto p-2 flex w-full justify-center items-center gap-x-1.5 text-white">
+                                <button className="text-center rounded-[4px] bg-[#282828] grow shrink h-[35px]" onClick={()=>setIsVaultBoxOpen(true)}>Add vault</button>
+                                <button onClick={()=>setVaultBoxes((currVaults)=> [])} className="DeleteAllBtn p-2 flex items-center justify-center grow shrink form-btn xl:h-[35px] bg-red-500 hover:font-semibold rounded-[4px]"><MdDeleteSweep/></button>
+                            </div>
+                        </div>
+                    )}
 
+                    {status === "empty" && (
+                            <div className="NoVaultParent w-full h-auto mt-34">
+                                <div className="NoVaultBox h-72 w-96 mx-auto">
+                                    <div className="UpperBar pt-2 px-2 w-full h-14 flex items-center justify-end border-2 border-black bg-linear-90 from-[#1a4bbf] to-[#020887]">
+                                        <svg width="35" height="43" viewBox="0 0 35 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M17.5 0.5C26.8757 0.5 34.5 8.32165 34.5 18C34.5 27.6784 26.8757 35.5 17.5 35.5C8.12434 35.5 0.5 27.6784 0.5 18C0.5 8.32165 8.12434 0.5 17.5 0.5Z" fill="#F4FFAA" stroke="#F4FFAA"/>
+                                            <path d="M15.4229 27V18.1577L14.6029 17.652V16.1897L17.5413 15.3833H19.8236V27H15.4229ZM17.4046 14.126L15.1633 11.871V11.3927L17.4046 9.15133H17.9239L20.2609 11.3927V11.871L17.9239 14.126H17.4046Z" fill="#0B0C00"/>
+                                        </svg>
+                                    </div>
+                                    <div className="dialog h-32 w-full text-center border-2 border-black rounded-b-lg pt-8 bg-linear-90 from-[#1a4bbf] to-[#020887_97.12%]">
+                                        <h2 className="jersey-25 text-gray-950">No vaults to be found here</h2>
+                                        <p className="hover:text-white text-white md:text-black hover:font-medium inline" onClick={()=>{return setIsVaultBoxOpen(true)}}>Click to get started</p>
+                                    </div>
+                                </div>
+                            </div>
+                    )}
+
+                    {status === "error" && (
+                                    <div className="NoVaultParent w-full h-auto mt-34 text-white">
+                                        <div className="NoVaultBox h-72 w-96 mx-auto">
+                                            <div className="UpperBar pt-2 px-2 w-full h-14 flex items-center justify-end border-2 border-black bg-linear-90 from-[#1a4bbf] to-[#020887]">
+                                                <svg width="35" height="43" viewBox="0 0 35 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M17.5 0.5C26.8757 0.5 34.5 8.32165 34.5 18C34.5 27.6784 26.8757 35.5 17.5 35.5C8.12434 35.5 0.5 27.6784 0.5 18C0.5 8.32165 8.12434 0.5 17.5 0.5Z" fill="#F4FFAA" stroke="#F4FFAA"/>
+                                                    <path d="M15.4229 27V18.1577L14.6029 17.652V16.1897L17.5413 15.3833H19.8236V27H15.4229ZM17.4046 14.126L15.1633 11.871V11.3927L17.4046 9.15133H17.9239L20.2609 11.3927V11.871L17.9239 14.126H17.4046Z" fill="#0B0C00"/>
+                                                </svg>
+                                            </div>
+                                            <div className="dialog h-32 w-full text-center border-2 border-black rounded-b-lg pt-8">
+                                                <h2 className="jersey-25">There was an error in retrieving the vaults</h2>
+                                                <p>Please try again later</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                    )}
+
+                </div>  
             </div>
         
             <DialogBox
