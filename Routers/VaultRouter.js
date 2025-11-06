318d1f721c04098fe3161276133de388410c0c53
Shreshth <shreshthkanchan777@gmail.com>
2025-11-07 00:12:51 +0530

    Served two more controllers on the vault routers, one for encrypting credentials and the other one to delete the specified vault from the database

--- Routers/VaultRouter.js
+++ Routers/VaultRouter.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-const {GetVaults, CreateVault} = require("../Controllers/VaultController")
+const {GetVaults, CreateVault, EncryptCreds, DeleteVault} = require("../Controllers/VaultController")
 
 router.get("/getVaults", GetVaults);
-router.post("/createVault", CreateVault)
+router.post("/createVault", CreateVault);
+router.get("/encryptCreds", EncryptCreds);
+router.delete("/deleteVault", DeleteVault);
+
 
 module.exports = router;
