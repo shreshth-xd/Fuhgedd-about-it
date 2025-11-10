20e7952d1fd97cfbbb0f840e8095f8e529742317
Shreshth <shreshthkanchan777@gmail.com>
2025-11-10 16:09:48 +0530

    Shifted the encrypt creds functions to a different controller

--- Routers/VaultRouter.js
+++ Routers/VaultRouter.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-const {GetVaults, CreateVault, EncryptCreds, DeleteVault} = require("../Controllers/VaultController")
+const {GetVaults, CreateVault, DeleteVault} = require("../Controllers/VaultController")
 
 router.get("/getVaults", GetVaults);
 router.post("/createVault", CreateVault);
-router.post("/encryptCreds", EncryptCreds);
+// router.post("/encryptCreds", EncryptCreds);
 router.delete("/deleteVault/:id", DeleteVault);
 
 
