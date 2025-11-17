1c3338b2d1126f89c638e8051112b893627c3acf
Shreshth <shreshthkanchan777@gmail.com>
2025-11-18 01:21:24 +0530

    Mistakenly imported JWT as an object and forgot to return the redirect on detecting that the user ain't got no token

--- Middlewares/Authentication.js
+++ Middlewares/Authentication.js
@@ -1,15 +1,16 @@
-const {jwt} = require("jsonwebtoken")
+const jwt = require("jsonwebtoken")
 const {setUser, getUser} = require("../Services/JWTAuth")
 
 async function RestrictToLoggedInUsersOnly(req, res, next){
     const token = req.cookies?.JWT_token;
-    if(!token) res.redirect("http://localhost:5173/sign-in")
+    if(!token) return res.redirect("http://localhost:5173/sign-in")
     
     try{
         let decodedPayload = jwt.verify(token, process.env.JWT_SIG)
         req.user = decodedPayload;
         next()
     }catch(error){
+        console.log(error)
         return res.status(403).json({"Status":"Invalid or expired token"})
     }
 }
