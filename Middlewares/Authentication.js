fddb887887d4423ba3d06188b481973865dcec47
Shreshth <shreshthkanchan777@gmail.com>
2025-11-22 00:31:51 +0530

    Removed repetitive logs

--- Middlewares/Authentication.js
+++ Middlewares/Authentication.js
@@ -3,16 +3,14 @@ const {setUser, getUser} = require("../Services/JWTAuth")
 
 async function RestrictToLoggedInUsersOnly(req, res, next){
     const token = req.cookies?.JWT_token;
-    if(!token) return res.redirect("http://localhost:5173/sign-in")
+    if(!token) return res.status(404).json({"Status":"User not found"})
     
     try{
         let decodedPayload = jwt.verify(token, process.env.JWT_SIG)
-        console.log("User found and verified")
         req.user = decodedPayload;
         next()
     }catch(error){
         console.log(error)
-        console.log("Couldn't verify the user")
         return res.status(403).json({"Status":"Invalid or expired token"})
     }
 }
