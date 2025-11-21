07baca3ee7bfc02d32fdd829a9d861a514334985
Shreshth <shreshthkanchan777@gmail.com>
2025-11-22 00:34:29 +0530

    Added two endpoints, one for verification and one for logout

--- Routers/UsersRouter.js
+++ Routers/UsersRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {signUp, signIn} = require("../Controllers/UserControllers");
+const {signUp, signIn, logout} = require("../Controllers/UserControllers");
 const app = express();
 const router = express.Router();
 const {RestrictToLoggedInUsersOnly} = require("../Middlewares/Authentication")
@@ -9,5 +9,6 @@ router.post("/sign-in", signIn);
 router.get("/verify", RestrictToLoggedInUsersOnly, (req, res)=>{
     return res.status(200).json({"Status":"Verified successfully"})
 }) 
+router.get("/logout", logout)
 
 module.exports = router;
