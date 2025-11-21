efaf69f36b4e0998ea8b48ad3c50d58aaedacdab
Shreshth <shreshthkanchan777@gmail.com>
2025-11-21 23:42:35 +0530

    Will attach the auth middleware once again as now I have fixed it correctly this time

--- Routers/VaultRouter.js
+++ Routers/VaultRouter.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const app = express();
 const router = express.Router();
-const {GetVaults, CreateVault, DeleteVault} = require("../Controllers/VaultController")
+const {GetVaults, CreateVault, DeleteVault, DeleteAllVaults} = require("../Controllers/VaultController")
+const {RestrictToLoggedInUsersOnly} = require("../Middlewares/Authentication")
 
-router.get("/getVaults", GetVaults);
+router.get("/getVaults", RestrictToLoggedInUsersOnly, GetVaults);
 router.post("/createVault", CreateVault);
 // router.post("/encryptCreds", EncryptCreds);
 router.delete("/deleteVault/:id", DeleteVault);
-
+router.delete("/deleteAllVaults", DeleteAllVaults);
 
 module.exports = router;
