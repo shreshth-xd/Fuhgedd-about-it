d8ff3258721c8de333db95c7657bcfacfa1ae7f2
Shreshth <shreshthkanchan777@gmail.com>
2025-11-21 23:42:58 +0530

    The auto login feature

--- Routers/UsersRouter.js
+++ Routers/UsersRouter.js
@@ -2,8 +2,12 @@ const express = require("express");
 const {signUp, signIn} = require("../Controllers/UserControllers");
 const app = express();
 const router = express.Router();
+const {RestrictToLoggedInUsersOnly} = require("../Middlewares/Authentication")
 
 router.post("/sign-up", signUp);
 router.post("/sign-in", signIn);
+router.get("/verify", RestrictToLoggedInUsersOnly, (req, res)=>{
+    return res.status(200).json({"Status":"Verified successfully"})
+}) 
 
 module.exports = router;
