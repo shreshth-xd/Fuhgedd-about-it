39fcd22a7795830a828ca009d7efab8907a0e968
Shreshth <shreshthkanchan777@gmail.com>
2025-12-05 23:56:01 +0530

    Fixed to /GetUser

--- Routers/UsersRouter.js
+++ Routers/UsersRouter.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const {signUp, signIn, getUser, logout} = require("../Controllers/UserControllers");
+const {signUp, signIn, GetUser, logout} = require("../Controllers/UserControllers");
 const app = express();
 const router = express.Router();
 const {RestrictToLoggedInUsersOnly} = require("../Middlewares/Authentication");
 
-router.get("/getUser", getUser);
+router.get("/getUser", GetUser);
 router.post("/sign-up", signUp);
 router.post("/sign-in", signIn);
 router.get("/verify", RestrictToLoggedInUsersOnly, (req, res)=>{
