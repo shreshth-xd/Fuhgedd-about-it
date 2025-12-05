8a4b676ba1089e91656342ff4f38613334082aa7
Shreshth <shreshthkanchan777@gmail.com>
2025-12-05 22:57:45 +0530

    Added this as a new button to create a new cred on an already created vault

--- Frontend/src/Components/DialogBox.jsx
+++ Frontend/src/Components/DialogBox.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const DialogBox = ({ isOpen, onClose, children }) => {
+const DialogBox = ({ isOpen, onClose, children, title = "Vault details" }) => {
     if (!isOpen) return null;
 
     return (
@@ -10,7 +10,7 @@ const DialogBox = ({ isOpen, onClose, children }) => {
                 {/* Dialog box */}
             <div className="bg-[#100f0f] text-white p-5 rounded-xl shadow-2xl w-2/4 relative border-2 border-[#c3bebe]">
                 <div className="DialogHeader w-full flex items-center justify-between mb-5">
-                    <h1 className="text-2xl">Vault details</h1>
+                    <h1 className="text-2xl">{title}</h1>
                     <button onClick={onClose} className="hover:font-semibold px-2.5 py-1 rounded-2xl bg-white text-black lg:text-white lg:bg-black/50 hover:bg-white hover:text-black">
                         ✕
                     </button>
